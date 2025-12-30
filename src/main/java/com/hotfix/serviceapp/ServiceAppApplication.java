02bb87bca68e911b91dc668f8f5c244ce865b7c6
Josh Rimes - Desktop <joshjohn1104@gmail.com>
2025-12-30 15:18:16 +0000

    Added spring starter dependencies and created data source postgreSQL connection

--- src/main/java/com/hotfix/serviceapp/ServiceAppApplication.java
+++ src/main/java/com/hotfix/serviceapp/ServiceAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class ServiceAppApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(ServiceAppApplication.class, args);
 	}
 
