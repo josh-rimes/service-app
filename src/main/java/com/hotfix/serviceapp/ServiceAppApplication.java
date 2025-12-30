b5c7ed2c7abc3d6552eca4c9caf6029629ab95b5
Josh Rimes - Desktop <joshjohn1104@gmail.com>
2025-12-30 14:16:48 +0000

    More springboot setup!

--- src/main/java/com/hotfix/serviceapp/ServiceAppApplication.java
+++ src/main/java/com/hotfix/serviceapp/ServiceAppApplication.java
@@ -2,8 +2,9 @@ package com.hotfix.serviceapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ServiceAppApplication {
 
 	public static void main(String[] args) {
