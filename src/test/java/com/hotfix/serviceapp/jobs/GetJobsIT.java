22e7ead0d0a16bc44a8543a98a03550fbacab62d
Josh Rimes - Desktop <joshjohn1104@gmail.com>
2026-02-11 21:08:15 +0000

    Code cleanup

--- src/test/java/com/hotfix/serviceapp/jobs/GetJobsIT.java
+++ src/test/java/com/hotfix/serviceapp/jobs/GetJobsIT.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class GetJobsIT extends BaseIT {
